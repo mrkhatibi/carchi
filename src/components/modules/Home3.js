a494208c4997c95dac13d8ac9d124f8526513848
Mojtaba Khatibi <Khatibi486@gmail.com>
2025-10-15 16:26:20 +0330

    fix bugs

--- src/components/modules/Home3.js
+++ src/components/modules/Home3.js
@@ -10,7 +10,7 @@ function Home3() {
   if (isLoading) return <Loader />;
   const showPosts = data.sort(() => 0.5 - Math.random()).slice(0, 8);
   return (
-    <div className="m-4 flex flex-col items-center w-full ">
+    <div className="flex flex-col items-center w-full ">
       <h3 className="text-4xl font-bold text-black drop-shadow-lg">وبلاگ ما</h3>
       <div className=" m-4 grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {showPosts.map((item) => (
@@ -18,7 +18,7 @@ function Home3() {
         ))}
       </div>
       <Link href={"/blogs"}>
-        <button className="bg-blue-700 p-7 cursor-pointer rounded-xl text-white shadow-2xl transition-transform duration-500 ease-in-out hover:scale-110">
+        <button className="bg-blue-700 p-7 mb-3 cursor-pointer rounded-xl text-white shadow-2xl transition-transform duration-500 ease-in-out hover:scale-110">
           پست های بیشتر
         </button>
       </Link>
